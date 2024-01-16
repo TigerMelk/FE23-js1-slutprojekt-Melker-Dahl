7cab2bff3b109f1d57a4166480024f49963bd51b
Melker Dahl <jaghetermelker@gmail.com>
2024-01-16 11:36:16 +0100

    day2-1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { fetchMovie } from "./modules/fetch.js";
 import { displayMovie, displayActor, displayError } from "./modules/display.js";
 const formEl = document.querySelector("form");
-
+console.log(fetchMovie);
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
   const userInput = document.querySelector("#textInput").value;
@@ -14,5 +14,8 @@ formEl.addEventListener("submit", (event) => {
   } else if (searchType == "Person") {
     fetchMovie(searchType, userInput).then(displayActor).catch(displayError);
   }
+  console.log(userInput);
+  console.log(searchType);
+  console.log(fetchMovie);
   formEl.reset();
 });
--- modules/display.js
+++ modules/display.js
@@ -1,5 +1,37 @@
-function displayMovie() {}
+// const mainDiv = document.querySelector("main");
+const mainContainer = document.querySelector("#mainContainer");
+const imgBase = `https://image.tmdb.org/t/p/`;
+const imgBackdropSize = "w400/";
+
+function displayMovie(movie) {
+  for (const movieList of movie.result) {
+    console.log(movieList);
+    const movieCardDiv = document.createElement("div");
+    createAndAppendEl(
+      "img",
+      imgBase + imgBackdropSize + movieList.poster_path,
+      movieCardDiv
+    );
+    createAndAppendEl("h2", movieList.title, movieCardDiv);
+    createAndAppendEl("p", movieList.release_date, movieCardDiv);
+    createAndAppendEl("p", movieList.overview, movieCardDiv);
+
+    mainContainer.append(movieCardDiv);
+  }
+}
 function displayActor() {}
 function displayError() {}
+
+function createAndAppendEl(type, content, container) {
+  const element = document.createElement(type);
+  container.append(element);
+
+  if (type === "img") {
+    element.src = content;
+  } else {
+    element.innerHTML = content;
+  }
+  return element;
+}
 
 export { displayMovie, displayActor, displayError };
